84900064101ad25462a8566e1d1a5e5da07edf8e
Pablo Murck <pablomurck@gmail.com>
2016-09-30 22:20:26 -0300

    Rearmar BFS heredando de Path

--- graph_paths/bfs.py
+++ graph_paths/bfs.py
@@ -1,66 +1,29 @@
-from queue import Queue
-from digraph import Digraph
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+from path import Path
+from collections import deque
 
 
-class Bfs:
-    def __init__(self, graph, root, dst):
-        self.graph = graph
-        self.root = root
-        self.dst = dst
+class Bfs(Path):
+    """
+    Breadth-first search
+    """
 
-        self.visit = []
-        self.paths = {}
-        self.distances = {}
+    def _algorithm(self):
+        q = deque()
 
-        q = Queue()
+        q.append(self.src)
+        self.distances[self.src] = 0
+        self.tagged[self.src] = True
 
-        q.queue(root)
-        self.distances[root] = 0
-        found = False
-        while q and not found:
-            current = q.unqueue()
-            for vertex in graph.adj(current):
-                if vertex not in self.visit:
-                    q.queue(vertex)
-                    self.paths[vertex] = current
-                    self.distances[vertex] = self.distances[current] + 1
-                    self.visit.append(vertex)
-                if vertex == dst:
-                    found = True
-                    break
+        while q and not self.tagged[self.dst]:
+            vertex = q.popleft()
+            for edge in self.graph.adj_e(vertex):
+                if not self.tagged[edge.dst]:
+                    q.append(edge.dst)
+                    self.edge_to[edge.dst] = edge
+                    self.distances[edge.dst] = self.distances[vertex] + edge.weight
+                    self.tagged[edge.dst] = True
 
-    def visited(self, vertex):
-        return vertex in self.visit
-
-    def distance(self, vertex):
-        """Returns distance found to vertex, or inf if vertex was not
-         found"""
-        if vertex not in self.distances:
-            return float("inf")
-        else:
-            # Esto es mas intricando de lo que me gustaria
-            # pero no quiero meter mano en la API del grafo
-            distance = 0
-            path = self.path(self.dst)
-            for i in range(len(path)):
-                for edge in self.graph.adj_e(path[i]):
-                    if edge.get_dst() == path[i+1]:
-                        distance += edge.get_weight()
-            return distance
-
-
-    def path(self, vertex):
-        """Returns list with the path found to vertex, none if there is
-        no such path, or an empty list if vertex is the root"""
-        if vertex == self.root:
-            return []
-        elif vertex not in self.visit:
-            return None
-        else:
-            path = []
-            current = vertex
-            path.insert(0, current)
-            while current in self.paths:
-                current = self.paths[current]
-                path.insert(0, current)
-        return path
+                    if edge.dst == self.dst:
+                        break
