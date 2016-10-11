43de5e977220d2cd225c970b4a60007aed721c9e
chrishaig14 <chrishaig14@gmail.com>
2016-10-11 16:05:30 -0300

    test con grafo random

--- graph_paths/search_test.py
+++ graph_paths/search_test.py
@@ -6,20 +6,22 @@ import random
 
 def basic_test(Class):
     print Class, "test"
-    graph = Digraph(7)
+    graph = Digraph(50)
 
-    for i in range(6):
-        l = range(i*1000000,(i+1)*1000000)
+    for i in range(50):
+        l = range(1,20)
         random.shuffle(l)
-        for j in l:
-            graph.add_edge(i,i+1, weight = j + 1)
+        dest = range(50)
+        dest.remove(i)
+        for w in l:
+            graph.add_edge(i,random.choice(dest), weight = w)
     print "Graph generated, starting algorithm:", Class
     start = timeit.default_timer()
-    search = Class(graph, 0, 6)
+    search = Class(graph, 0, 49)
 
     end = timeit.default_timer()
     print "total time: ", end - start
-    for i in range(7):
+    for i in range(50):
         p = search.path(i)
         print "path to ", i, " :"
         if p:
